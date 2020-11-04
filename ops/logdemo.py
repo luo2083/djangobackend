a801da13fe382cca8d3754bac7f6145a54cf0bbe
luo2083 <1354029556@qq.com>
2020-11-04 10:18:10 +0800

    modify logdemo.

--- ops/logdemo.py
+++ ops/logdemo.py
@@ -1,8 +1,20 @@
+import os
+import django
 import logging
 
-class TestFilter(logging.Filter):
-    def filter(self, record):
-        if '------' in record.msg:
-            return False
-        else:
-            return True
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+
+def logger_demo():
+    logger = logging.getLogger('django')
+    logger.info('hello world')
+    logger.info('hello world [FILTER FLAG]')
+
+    logger.debug('debug message')
+
+    logger.error('error message')
+
+
+if __name__ == '__main__':
+    logger_demo()
