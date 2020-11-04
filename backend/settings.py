a084328360d3c92c939b1eaf05eba89fe18c78d7
luo2083 <1354029556@qq.com>
2020-11-04 11:35:47 +0800

    modify middleware config 4.

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django_crontab',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
